603ac829f47bb29dd5bff94c86d0ee27ef7626d5
Luís <luis.eduardo.rozante@gmail.com>
2019-08-28 00:04:56 -0300

    Improved movimentation.

--- Assets/Scripts/Entities/Player.cs
+++ Assets/Scripts/Entities/Player.cs
@@ -12,9 +12,18 @@ namespace DEEP.Entities
     {
 
         [Header("Movimentation")]
-        [SerializeField] private float maxVelocity = 6f;
-        [SerializeField] private float acceleration = 2f;
 
+        [Tooltip("Max velocity the Player can reach when walking.")]
+        [SerializeField] private float maxVelocity = 8f;
+
+        [Tooltip("Player acceleration.")]
+        [SerializeField] private float acceleration = 5f;
+
+        [Range(0,1)]
+        [Tooltip("Drag used to slow down the Player when no walking.")]
+        [SerializeField] private float groundDrag = 0.25f;
+
+        [Tooltip("How fast the Player should turn.")]
         [SerializeField] private Vector2 mouseSensitivity = new Vector2(1, 1);
 
         [Header("Weapons")]
@@ -22,8 +31,8 @@ namespace DEEP.Entities
         [Tooltip("Ammo sources carried by the player.")]
         public List<AmmoSource> ammoTypes;
 
-        private Rigidbody _rigidbody = null;
-        private Camera _camera = null;
+        private Rigidbody _rigidbody = null; // Player's Rigidbody.
+        private Camera _camera = null; // Player's Camera.
 
         protected override void Start()
         {
@@ -43,7 +52,7 @@ namespace DEEP.Entities
         {
 
             // Gets the input for the Player rotation.
-            Vector2 mouseInput = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"));
+            Vector2 mouseInput = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
 
             // Rotates the Player arount the y-axis.
             transform.transform.Rotate(new Vector3(0, mouseInput.x * 360 * mouseSensitivity.x * Time.deltaTime, 0));
@@ -62,13 +71,34 @@ namespace DEEP.Entities
         {
 
             // Gets the input for the Player movimentation.
-            Vector3 movInput = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+            Vector3 movInput = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
 
-            // Calculates the Player velocity on the x-z plane.
-            float planarVel = new Vector2(_rigidbody.velocity.x, _rigidbody.velocity.z).magnitude;
+            // Gets the local velocity of the Rigidbody.
+            Vector3 localVel = transform.InverseTransformDirection(_rigidbody.velocity);
+            Vector3 planeVel = localVel; planeVel.y = 0; // Gets the local velocity on the x-z plane.
 
-            // If the max velocity on the x-z plane has not yet been achieved, accelerates the Player.
-            if(planarVel < maxVelocity) _rigidbody.AddRelativeForce(movInput * acceleration * _rigidbody.mass, ForceMode.Impulse);
+            // Applies drag to the Rigidbody (this is done via script in order to not apply drag on the y-axis).
+            if(movInput.x == 0)
+                localVel.x *= (1 - groundDrag);
+            if(movInput.z == 0)
+                localVel.z *= (1 - groundDrag);
+
+            // Guarantees the Player velocity on the x-z plane never goes above maximum.
+            if(planeVel.magnitude > maxVelocity)
+            {
+                localVel.x = planeVel.normalized.x * maxVelocity; // Clamps the velocity.
+                localVel.z = planeVel.normalized.z * maxVelocity;
+            }
+
+            // Assigns the new velocity to the Player.
+            _rigidbody.velocity = transform.TransformDirection(localVel);
+
+            // Recalculates the velocity on the plane.
+            planeVel = localVel; planeVel.y = 0;
+
+            // Accelerates the Player based on input and if it won't go above the maximum velocity.
+            if((planeVel + movInput * acceleration * Time.fixedDeltaTime).magnitude < maxVelocity)
+                _rigidbody.AddRelativeForce(movInput * acceleration * _rigidbody.mass, ForceMode.Impulse);
 
         }
 
