1e458c6d12da4ccce6961dd8d2ace8b09a5b1565
Du <du@pop-os.localdomain>
2019-09-03 16:35:17 -0300

    Additions to weapons system.
    - Added weapon texture.
    
    - Added weapon aniamation and sound.
    
    Fixed weapon consuming ammo without waiting for firing delay.

--- Assets/Scripts/Weapons/SimpleWeapon.cs
+++ Assets/Scripts/Weapons/SimpleWeapon.cs
@@ -16,11 +16,28 @@ namespace DEEP.Weapons
         [SerializeField] protected float delayBetweenShots = 0.3f;
         private float delayTimer = 0; //Used to count the time between shots.
 
+        private Animator _animator; // Stores the weapon's Animator.
+        private AudioSource _audio; // Stores the weapon's AudioSource.
+
+        [Tooltip("AudioClip to be played when shooting.")]
+        [SerializeField] protected AudioClip shotClip = null;
+
+        [Tooltip("AudioClip to be played when trying to shoot whitout ammo.")]
+        [SerializeField] protected AudioClip noAmmoCLip = null;
+
         protected virtual void Start()
         {
 
             // Allows the weapon to be fired at start.
             delayTimer = delayBetweenShots;
+
+            // Gets the weapon's animator.
+            _animator = GetComponentInChildren<Animator>();
+            if(_animator == null) Debug.LogError("DEEP.Weapons.SimpleWeapon.Start: Animator not found!");
+
+            // Gets the weapon's AudioSource.
+            _audio = GetComponentInChildren<AudioSource>();
+            if(_audio == null) Debug.LogError("DEEP.Weapons.SimpleWeapon.Start: AudioSource not found!");
 
         }
 
@@ -33,22 +50,44 @@ namespace DEEP.Weapons
 
         }
 
+        // Attempts to fire the weapon.
+        public override void Shot()
+        {
+
+            // Verifies if the weapon can be fired.
+            if(delayTimer >= delayBetweenShots)
+                base.Shot();
+
+        }
 
         // Fires the weapon.
         protected override void Fire() 
         {
 
-            // Verifies if the weapon can be fired.
-            if(delayTimer >= delayBetweenShots)
-            {
-                Instantiate(bulletPrefab, bulletSpawn.position, bulletSpawn.rotation); // Creates the bullet.
-                delayTimer = 0; // Resets the delay.
-            }
+            Instantiate(bulletPrefab, bulletSpawn.position, bulletSpawn.rotation); // Creates the bullet.
+            delayTimer = 0; // Resets the delay.
+
+            // Plays the animation.
+            _animator.SetBool("Fire", true);
+            _animator.SetBool("NoAmmo", false);
+
+            // Plays the audio.
+            _audio.clip = shotClip;
+            _audio.Play();
 
         }
 
          // Do something when not enough ammo is avaliable.
-        protected override void NoAmmo() {}
+        protected override void NoAmmo() 
+        {
+
+            delayTimer = 0; // Resets the delay.
+
+            // Plays the audio.
+            _audio.clip = noAmmoCLip;
+            _audio.Play();
+
+        }
 
     }
 
--- README.md
+++ README.md
@@ -14,3 +14,6 @@ Deep is a game developed by Fellowship of the Game.
 ## Disclaimer
 
 * Some textures from textures.com were used
+* The following sounds were used in the game:
+    - https://freesound.org/people/LeMudCrab/sounds/163456/
+    - https://freesound.org/people/michorvath/sounds/427603/
