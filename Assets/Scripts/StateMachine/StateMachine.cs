77a07ee6fdbbdf6750bbd0c56058bbd974cdb179
vini <vini_carvalho99@hotmail.com>
2019-09-25 23:14:40 -0300

    basic AI and starting generic stateMachine

---
+++ Assets/Scripts/StateMachine/StateMachine.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DEEP.StateMachine
+{
+    public class StateMachine <T>{
+
+        public T Owner;
+        public State <T> currentState{get; private set;}
+
+        public StateMachine(T owner){
+            this.Owner =owner;
+            currentState =  null;
+        }
+
+        public void ChangeState(State<T> newState){
+            
+            if (newState != null)
+            {
+                if (currentState != null)
+                    currentState.ExitState(Owner);
+                currentState =  newState;
+                currentState.EnterState(Owner);
+            }
+        }
+
+        public void update(){
+            if (currentState != null)
+                currentState.UpdateState(Owner);
+        }
+
+    }
+
+    public class State <T>{
+
+        public abstract void EnterState(T owner);
+
+        public abstract void ExitState(T owner);
+        public abstract void UpdateState(T owner);
+    }
+}
---
+++ Assets/Scripts/Weapons/EnemyAmingSystem.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using DEEP.Weapons;
+
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class EnemyAmingSystem : MonoBehaviour
+{
+    [SerializeField]protected float radius; //search radius
+    [SerializeField]private WeaponBase weapon;
+    protected float Waiting_time;
+    protected float time;
+    protected GameObject target;
+
+    void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("Player");
+        weapon = GetComponentInChildren<WeaponBase>();
+
+    }
+
+    void Update()
+    {
+        Shooting();
+    }
+
+    public void Shooting(){
+        getAim();
+        if (weapon != null)
+            weapon.Shot();
+    }
+
+    public void Pursuing(){
+        getAim();
+        if (weapon != null)
+            weapon.Shot();
+    }
+
+    public void waiting(){
+        if (Vector3.Distance(transform.position, target.transform.position) <= radius)
+        {
+            
+        }
+    }
+
+    public void getAim(){
+
+        var pos = target.transform.position -transform.position;
+        pos.y = 0;// prevents the y axis rotation
+        var rotate = Quaternion.LookRotation(pos);
+        transform.rotation = Quaternion.Slerp(transform.rotation, rotate, Time.deltaTime * 10.0f);
+    }
+}
