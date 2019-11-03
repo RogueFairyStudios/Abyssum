8c3c7c583217f9a06635a1f14494460aee74c9cf
Óliver S Becker <oliverbecker@usp.br>
2019-11-03 17:41:04 -0300

    KeyCard System
    Adição das chaves e portas coloridas. A abertura
    da porta está apenas desativando o item, ainda
    falta criar a animação. Além disso, é preciso
    adicionar a layer "Door" para que funcione corre-
    tamente.

---
+++ Assets/Scripts/Keycards/ColorsDoor.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DEEP.KeyCard {
+	public class ColorsDoor : MonoBehaviour {
+		public KeysColors doorColor = 0;
+
+		public void TryOpenDoor() {
+			print("Trying to open the door");
+			if (InventoryKey.inventory.Contains(doorColor)) {
+				OpenDoor();
+			}
+		}
+
+		private void OpenDoor() {// Activates the OpenDoor animation
+			print("Opening the door");
+			gameObject.SetActive(false);
+		}
+	}
+}
