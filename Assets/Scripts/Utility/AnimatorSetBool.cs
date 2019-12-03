768afa99a34717c62b13a974d0d3e7721ca4010a
Luís Eduardo Rozante <luis.eduardo.rozante@gmail.com>
2019-12-03 13:52:37 -0300

    Crawler animations and animation test scene

---
+++ Assets/Scripts/Utility/AnimatorSetBool.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace DEEP.Utilty
+{
+
+    // Sets the bool of an animator, used to test animations.
+    public class AnimatorSetBool : MonoBehaviour
+    {
+
+        [SerializeField] string parameterName = "";
+        [SerializeField] bool parameterValue = true;
+
+        void Start()
+        {
+            
+            Animator animator = GetComponentInChildren<Animator>();
+            animator.SetBool(parameterName, parameterValue);
+
+        }
+
+    }
+}
