19a07d5daf2408e13ce0a5696709e2b90f2a67c0
Luís Eduardo Rozante <luis.eduardo.rozante@gmail.com>
2019-12-11 18:35:03 -0300

    Collectibles and HUD

---
+++ Assets/Scripts/Collectibles/AmmoCollectible.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using DEEP.Entities;
+
+namespace DEEP.Collectibles
+{
+    public class AmmoCollectible : CollectibleBase
+    {
+        [SerializeField] private string type = "Shell";
+        [SerializeField] private int amount = 10;
+
+        protected override void Collect(Player player) {
+
+            // Tries healing the player and stores if it was healed.
+            bool collected = player.GiveAmmo(amount, type, collectionSound);
+            
+            if(collected) {
+                
+                // Destroys the object if the collectible is used.
+                Destroy(gameObject);
+
+            }
+
+        }
+
+    }
+}
---
+++ Assets/Scripts/Collectibles/CollectibleBase.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using DEEP.Entities;
+
+namespace DEEP.Collectibles
+{
+    public abstract class CollectibleBase : MonoBehaviour
+    {
+
+        [SerializeField] protected AudioClip collectionSound = null;
+
+        protected virtual void OnTriggerEnter(Collider col)
+        {
+
+            Player player = col.GetComponent<Player>();
+            if (player != null)
+            {
+                
+                Collect(player);
+
+            }          
+
+        }
+
+        protected abstract void Collect(Player player);
+    }
+}
