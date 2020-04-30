51fceac41ee259ad30c6f161751e12574540196a
Luís Eduardo Rozante de Freitas Pereira <luis.eduardo.rozante@gmail.com>
2020-04-30 13:59:36 -0300

    Added new submarine and damage triggers

---
+++ Assets/Scripts/Utility/DamageTrigger.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+
+using System.Collections;
+
+using DEEP.Entities;
+
+namespace DEEP.Utility
+{
+
+    public class DamageTrigger : MonoBehaviour
+    {
+
+        private Player target;
+        private Coroutine damageCoroutine;
+
+        [SerializeField] private float delay = 0.2f;
+        [SerializeField] private int amount = 10;
+
+        void OnTriggerEnter(Collider other) 
+        {
+
+            // Checks if the player entered the trigger.
+            target = other.GetComponent<Player>();
+
+            // Does the damage.
+            if(target != null)
+                damageCoroutine = StartCoroutine(DamageOverTime());
+
+        }
+
+        void OnTriggerExit(Collider other) 
+        {
+
+            // Checks if the player exited the trigger.
+            target = other.GetComponent<Player>();
+
+            // Stop doing damage.
+            if (target != null)
+                StopCoroutine(damageCoroutine);
+        
+        }
+
+        // Does damage from time to time.
+        IEnumerator DamageOverTime()
+        {
+
+            float time = 0.0f;
+            while(true) 
+            {
+
+                time += Time.fixedDeltaTime;
+
+                if(time >= delay) {
+                    target.Damage(amount, DamageType.Regular);
+                    time = 0.0f;
+                }
+
+                yield return new WaitForFixedUpdate();
+
+            }
+
+        }
+
+    }
+}
