da153b7e6fb8346faaa5774b84927e9ad500fa72
vini <vini_carvalho99@hotmail.com>
2020-05-25 15:52:05 -0300

    some fixes

---
+++ Assets/Scripts/Spawner/spawnerManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawnerManager : MonoBehaviour
+{
+   // [SerializeField] private List<GameObject> spawnerList = new List<spawner>();
+    
+    public void createWave(){}
+
+    public void createWave(int time){}
+    
+
+}
