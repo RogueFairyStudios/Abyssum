3027e2eba5ea637fd2fa7abdda52c11c988c9709
vini <vini_carvalho99@hotmail.com>
2020-05-27 14:08:34 -0300

    spawner manager

--- Assets/Scripts/Spawner/spawner.cs
+++ Assets/Scripts/Spawner/spawner.cs
@@ -1,61 +1,67 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DEEP.AI;
 
-public class spawner : MonoBehaviour
+namespace DEEP.spawn
 {
-    [SerializeField] private List<GameObject> entitieList = new List<GameObject>();
-    public GameObject startingPoint;
-    public int i = 0;
-    float time = 2;
-    // Start is called before the first frame update
-    void Start()
+    public class spawner : MonoBehaviour
     {
-        spawnObjects();
-    }
-
-    void Update(){
-        time -= Time.deltaTime;
-        if(time <= 0){
-            spawnObject(i);
+        [SerializeField] private List<GameObject> entitieList = new List<GameObject>();
+        public GameObject startingPoint;
+        public int i = 0;
+        float time = 2;
         
-            i = (i+1)% entitieList.Count;
-            time = 2;
+        void Start()
+        {
+            spawnObjects();
+        }
+
+        void Update(){
+            time -= Time.deltaTime;
+            if(time <= 0){
+                spawnObject(i);
+            
+                i = (i+1)% entitieList.Count;
+                time = 2;
+            }
+
+        }
+
+        public void spawnObject(int id){
+
+            if (id < 0 || id > entitieList.Count)
+            {
+                Debug.Log("Invalid Id");
+                return;
+            }
+
+            GameObject instance = Instantiate(entitieList[id],this.transform);
+            instance.GetComponent<EnemyAISystem>().addPatrolPoint(startingPoint);
+            instance.GetComponent<EnemyAISystem>().ResetPatrol();
+        }
+
+        public void spawnNObject(int id, int n){
+
+            if (id < 0 || id > entitieList.Count)
+            {
+                Debug.Log("Invalid Id");
+                return;
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                spawnObject(id);    
+            }
+        }
+
+        public void spawnObjects(){
+            for (int i = 0; i < entitieList.Count; i++)
+            {
+                spawnObject(i);
+            }
         }
 
     }
-
-    public void spawnObject(int id){
-
-        if (id < 0 || id > entitieList.Count)
-        {
-            Debug.Log("Invalid Id");
-            return;
-        }
-
-        GameObject instance = Instantiate(entitieList[id],this.transform);
-        instance.GetComponent<EnemyAISystem>().addPatrolPoint(startingPoint);
-    }
-
-    public void spawnNObject(int id, int n){
-
-        if (id < 0 || id > entitieList.Count)
-        {
-            Debug.Log("Invalid Id");
-            return;
-        }
-
-        for (int i = 0; i < n; i++)
-        {
-            spawnObject(id);    
-        }
-    }
-
-    public void spawnObjects(){
-        for (int i = 0; i < entitieList.Count; i++)
-        {
-            spawnObject(i);
-        }
-    }
-
+    
 }
--- Assets/Scripts/Spawner/spawnerManager.cs
+++ Assets/Scripts/Spawner/spawnerManager.cs
@@ -2,13 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class spawnerManager : MonoBehaviour
+namespace DEEP.Spawn
 {
-   [SerializeField] private List<spawner> spawnerList = new List<spawner>();
+    public class spawnerManager : MonoBehaviour
+    {
+    [SerializeField] private List<spawner> spawnerList = new List<spawner>();
     
-    public void createWave(){}
+        public void createWave(int spawnerId, int enemyId, int enemyAmount){
+            if (spawnerId >= 0  && spawnerId< spawnerList.Count)
+                spawnerList.[spawnerId].spawnNObject(enemyId, enemyAmount);
+        }
 
-    public void createWave(int time){}
-    
-
+        public void createBigWave(int enemyId, int enemyAmount){
+            for (int i = 0; i < spawnerList.Count; i++)
+            {
+                createWave(i, enemyId, enemyAmount);
+            }
+        }
+    }
 }
