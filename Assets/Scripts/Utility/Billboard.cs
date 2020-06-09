7728f6ca2aee1598d456baee6c0b91c79659156f
Luís Eduardo Rozante de Freitas Pereira <luis.eduardo.rozante@gmail.com>
2020-06-09 11:24:31 -0300

    Improved effects

---
+++ Assets/Scripts/Utility/Billboard.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace DEEP.Utility
+{
+
+    // Always faces the main camera.
+    public class Billboard : MonoBehaviour
+    {
+
+        [Tooltip("Rotation offset to add to the billboard.")]
+        [SerializeField] protected Vector3 rotationOffset = Vector3.zero;
+
+        void Update()
+        {
+
+            transform.LookAt(Camera.main.transform);
+            transform.rotation *= Quaternion.Euler(rotationOffset);
+
+        }
+
+    }
+
+}
