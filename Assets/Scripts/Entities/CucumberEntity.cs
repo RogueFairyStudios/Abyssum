a073986df5b5b15305d00d423136860ca8182bef
Luís Eduardo Rozante de Freitas Pereira <luis.eduardo.rozante@gmail.com>
2020-06-09 13:21:28 -0300

    Fixed cucumber dying more than once with explosions

--- Assets/Scripts/Entities/CucumberEntity.cs
+++ Assets/Scripts/Entities/CucumberEntity.cs
@@ -18,11 +18,17 @@ namespace DEEP.Entities
         [Tooltip("The velocity in which the object is moved during the death effect.")]
         [SerializeField] protected float deathFadeVelocity = 0.1f;
 
+        // Used to mark the the fade death effect is happening.
+        private bool isDying;
+
         // Object used to wait in coroutines.
         private WaitForFixedUpdate waitForFixed = new WaitForFixedUpdate();
         protected override void Start() {
 
             base.Start();
+
+            // Not dying at start.
+            isDying = false;
 
             // Gets the necessary components at start.
             meshRenderer = GetComponentInChildren<SkinnedMeshRenderer>();
@@ -32,6 +38,9 @@ namespace DEEP.Entities
         }
 
         protected override void Die() {
+
+            // Ensures death doesn't happen twice.
+            if (isDying) return;
 
             // Hides the gameObject.
             meshRenderer.enabled = false;
@@ -49,6 +58,9 @@ namespace DEEP.Entities
 
             // Moves the object to "fade" the pool.
             StartCoroutine(DyingFade());
+
+            // Marks as dead.
+            isDying = true;
 
         }
 
