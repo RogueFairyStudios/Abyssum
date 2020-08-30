510c9869d94ed41d025a53471e8b1440b55eeb1a
Abner Santos <abner.santos@usp.br>
2020-08-30 09:55:01 -0300

    Experimenting with new menu style

---
+++ Assets/Scripts/UI/MenuCutscenes.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Playables;
+
+public class MenuCutscenes : MonoBehaviour
+{
+    [SerializeField] Object sceneToLoad;
+    [SerializeField] GameObject background;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //! Temp for now
+        SceneManager.LoadSceneAsync(sceneToLoad.name, LoadSceneMode.Additive);
+        SceneManager.sceneLoaded += FindDirector;
+    }
+
+    void FindDirector(Scene scene, LoadSceneMode mode)
+    {
+        background.SetActive(false);
+
+        //! FIXME: This is utterly disgusting. Or not. I mean, it's good for performance since it's literally the first object there but it's ugly to look at.
+        //! Just like that friend of yours, bob. Y'know, he's cool and all but you just feel weird when you look at his face. Poor bob. Maybe the problem is us after all.
+        PlayableDirector director = scene.GetRootGameObjects()[0].GetComponent<PlayableDirector>();
+        // TODO as soon as cutscene ends, select another one
+    }
+
+    private void OnDisable()
+    {
+        SceneManager.sceneLoaded -= FindDirector;
+    }
+}
